24e95b6fc24f01afb7fb3e58969019dde532727e
daniel garcia <dany2017305@gmail.com>
2025-05-28 19:56:23 -0500

    feat: crea el componente ButtonForm

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import '../style.css'
+import { ButtonForm } from './ButtonForm'
+
+function App () {
+  return (
+    <>
+      <ButtonForm titleForm='' />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ButtonForm.jsx
@@ -0,0 +1,9 @@
+import '../style.css'
+
+function ButtonForm ({ titleForm }) {
+  return (
+    <button className='bg-red-400'>{titleForm}</button>
+  )
+}
+
+export { ButtonForm }
